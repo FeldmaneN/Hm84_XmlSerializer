92933d5afa0b324ab5d393426e6ef1dfb96f54d8
E5-573G <E5-573G@E5-573G>
2022-10-16 18:21:02 +0300

    Add project files.

---
+++ Hm84XmlSerializer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System;
+using System.Xml.Linq;
+
+namespace Hm84XmlSerializer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Note myNote = new Note();
+
+            myNote.To = "Tove";
+            myNote.From = "Jani";
+            myNote.Heading = "Reminder";
+            myNote.Body = "Don't forget me this weekend!";
+
+            XmlHelper.Save(myNote);
+
+            var loadedNote = XmlHelper.Load();
+        }
+    }
+}
---
+++ Hm84XmlSerializer/XmlHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Hm84XmlSerializer
+{
+    internal class XmlHelper
+    {
+        internal static void Save(Note note)
+        {
+            XmlSerializer serlzr = new XmlSerializer(typeof(Note));
+            using(var writer = new FileStream(@"singlenote.xml", FileMode.Create))
+            {
+                serlzr.Serialize(writer, note);
+            }            
+        }
+        internal static Note Load()
+        {
+            XmlSerializer deserlzr = new XmlSerializer(typeof(Note));
+
+            using (var reader = new StreamReader(@"singlenote.xml"))
+            {
+                var note = deserlzr.Deserialize(reader);
+                return (Note)note;
+            }
+        }
+    }
+}
